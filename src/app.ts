3d64502bcdbe87d252bf621be9f95b0051fd79c9
lexky82 <lexky@naver.com>
2024-10-05 17:03:03 +0900

    feat: session, staic, morgan 미들웨어 추가

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,27 @@
 import express, { Application } from "express";
 import routes from "./routes";
+import path from "path";
+import session from "express-session";
+import morgan from "morgan";
 
 const app: Application = express();
 
+declare module "express-session" {
+  export interface SessionData {
+    verifiedEmail?: string | undefined;
+  }
+}
+
+app.use(
+  session({
+    secret: "hidden_key",
+    resave: false,
+    saveUninitialized: true,
+    cookie: { secure: false },
+  })
+);
+app.use("/static", express.static(path.join(process.cwd(), "public")));
+app.use(morgan("combined"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
