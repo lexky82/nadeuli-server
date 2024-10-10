3c286d3d426ec5fb7e34003936bd66d8aff87ec1
lexky82 <lexky@naver.com>
2024-10-10 13:06:59 +0900

    feat: 시퀄라이즈 DB 덮어쓰는 옵션 제거

--- src/lib/sync.ts
+++ src/lib/sync.ts
@@ -2,7 +2,7 @@ import sequelize from "./sequelize";
 
 const syncDatabase = async () => {
   try {
-    await sequelize.sync({ force: true });
+    await sequelize.sync();
     console.log("Database synchronized successfully.");
   } catch (error) {
     console.error("Unable to synchronize the database:", error);
