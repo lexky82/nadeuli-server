6145d71a22483e437f4d3564b6a42261515fe57f
lexky82 <lexky@naver.com>
2024-10-10 13:06:37 +0900

    feat: login, isValidEmail, refreshAccessToken, logout 컨트롤러 추가 및 sendEmailVerification 이메일 중복 체크 로직 추가

--- src/controllers/auth.ts
+++ src/controllers/auth.ts
@@ -1,7 +1,7 @@
-import { Request, Response } from "express";
+import { NextFunction, Request, Response } from "express";
 import bcrypt from "bcrypt";
 import User from "../models/User";
-import { sign, verify } from "jsonwebtoken";
+import { JwtPayload, VerifyErrors, sign, verify } from "jsonwebtoken";
 import nodemailer from "nodemailer";
 import dotenv from "dotenv";
 import path from "path";
@@ -10,34 +10,132 @@ import fs from "fs/promises";
 dotenv.config();
 
 export const register = async (req: Request, res: Response) => {
-  const { email, phone, nickName, password } = req.body;
+  const { email, nickName, password } = req.body;
 
   try {
-    const emailVerification = req.session.verifiedEmail;
-
-    if (!emailVerification) {
-      res.status(400).json({ error: "이메일 인증을 완료해야 합니다!" });
+    if (!req.session.verifiedEmail) {
+      res.status(400).json({ message: "이메일 인증을 완료해야 합니다!" });
+      return;
     }
 
     const hashedPassword = await bcrypt.hash(password, 10);
 
     const newUser = await User.create({
       email,
-      phone,
       nickName,
       password: hashedPassword,
     });
 
     res
       .status(201)
-      .json({ message: "User registered successfully", user: newUser });
+      .json({ message: "회원가입을 성공하였습니다.", user: newUser });
   } catch (error) {
     res.status(500).json({ message: "Registration failed", error });
   }
 };
 
+export const login = async (req: Request, res: Response) => {
+  const { email, password } = req.body;
+
+  try {
+    const user = await User.findOne({ where: { email } });
+
+    if (!user) {
+      res.status(400).json({ message: "존재하지 않는 이메일입니다." });
+      return;
+    }
+
+    const isMatch = await bcrypt.compare(password, user.password);
+
+    if (!isMatch) {
+      res.status(400).json({ message: "잘못된 비밀번호입니다." });
+      return;
+    }
+
+    const accessToken = sign(
+      { id: user.id, email: user.email },
+      process.env.JWT_SECRET as string,
+      { expiresIn: "1h" }
+    );
+
+    const refreshToken = sign(
+      { id: user.id, email: user.email },
+      process.env.JWT_SECRET as string,
+      { expiresIn: "7d" }
+    );
+
+    res.cookie("accessToken", accessToken, {
+      httpOnly: true,
+      secure: true,
+      maxAge: 3600000,
+      sameSite: "strict",
+    });
+
+    res.cookie("refreshToken", refreshToken, {
+      httpOnly: true,
+      secure: true,
+      maxAge: 7 * 24 * 60 * 60 * 1000, // 7일
+      sameSite: "strict",
+    });
+
+    res.status(200).json({
+      ...{ ...user, accessToken, refreshToken },
+    });
+  } catch (error) {
+    res.status(500).json({ message: "로그인 실패" });
+  }
+};
+
+export const refreshAccessToken = async (req: Request, res: Response) => {
+  const refreshToken = req.cookies.refreshToken;
+
+  if (!refreshToken) {
+    res.status(401).json({ message: "리프레시 토큰이 없습니다." });
+    return;
+  }
+
+  try {
+    const decoded = verify(refreshToken, process.env.JWT_SECRET as string) as {
+      id: string;
+      email: string;
+    };
+
+    const newAccessToken = sign(
+      { id: decoded.id, email: decoded.email },
+      process.env.JWT_SECRET as string,
+      { expiresIn: "1h" }
+    );
+
+    res.cookie("accessToken", newAccessToken, {
+      httpOnly: true,
+      secure: true,
+      maxAge: 3600000, // 1시간
+      sameSite: "strict",
+    });
+
+    res.status(200).json({ message: "액세스 토큰 갱신 성공" });
+  } catch (error) {
+    res
+      .status(403)
+      .json({ message: "유효하지 않은 리프레시 토큰입니다.", error });
+  }
+};
+
+export const logout = (req: Request, res: Response) => {
+  res.clearCookie("accessToken");
+  res.clearCookie("refreshToken");
+  res.status(200).json({ message: "로그아웃 성공" });
+};
+
 export const sendEmailVerification = async (req: Request, res: Response) => {
   const { email } = req.body;
+
+  const existingUser = await User.findOne({ where: { email: email } });
+
+  if (existingUser) {
+    res.status(409).json({ message: "이미 존재하는 사용자입니다." });
+    return;
+  }
 
   const token = sign({ email }, process.env.JWT_SECRET as string, {
     expiresIn: "1h",
@@ -85,6 +183,7 @@ export const verifyEmail = async (req: Request, res: Response) => {
 
   if (!token) {
     res.status(400).json({ error: "Token is required" });
+    return;
   }
 
   try {
@@ -93,9 +192,43 @@ export const verifyEmail = async (req: Request, res: Response) => {
 
     req.session.verifiedEmail = email;
 
-    res.status(200).json({ message: "Email verified successfully", email });
+    res.send(`
+    <html>
+      <body>
+        <script>
+          alert('이메일 인증이 완료되었습니다.');
+          window.close();
+        </script>
+      </body>
+    </html>
+  `);
   } catch (error) {
     console.error("Invalid token:", error);
-    res.status(400).json({ error: "Invalid or expired token" });
+    res.status(400).send(`
+      <html>
+        <body>
+          <script>
+            alert('이메일 인증에 실패했습니다. 다시 시도해주세요.');
+            window.close();
+          </script>
+        </body>
+      </html>
+    `);
+  }
+};
+
+export const isValidEmail = async (req: Request, res: Response) => {
+  const { email } = req.query;
+
+  try {
+    if (req.session.verifiedEmail !== email) {
+      new Error("인증받지 않는 이메일입니다.");
+      return;
+    }
+
+    res.status(200).json({ message: "인증이 완료된 이메일입니다.", email });
+  } catch (error) {
+    console.error(error);
+    res.status(401).json({ message: error, email });
   }
 };
